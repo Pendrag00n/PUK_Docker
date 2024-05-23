22cdbacd24ab631e5f506941b4a369d233c0160a
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2024-05-23 13:19:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo " ** Installing docker-compose"
+sudo apt install docker-compose -y
 echo " ** Building the docker image and starting the container..."
 docker-compose up --build
 if [ $? -eq 0 ]; then
