c4a62038ee20a522967aa9791f8aa96d1e0dff15
Pendrag00n <juliofelipeHI@gmail.com>
2024-05-27 02:12:04 +0100

    Updated files

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-echo " ** Installing docker-compose"
-sudo apt install docker-compose -y
-echo " ** Building the docker image and starting the container..."
-docker-compose up --build
-if [ $? -eq 0 ]; then
-    echo " ** Docker container started successfully."
+
+echo " ** Testing docker installation"
+if [ -x "$(which docker)" ]; then
+    echo " ** docker is already installed"
 else
-    echo " ** Docker container might've failed to start."
+    echo " ** Installing docker"
+    curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
+fi
+echo " ** Testing docker-compose installation"
+if [ -x "$(which docker-compose)" ]; then
+    echo " ** docker-compose is already installed"
+else
+    echo " ** Installing docker"
+    sudo apt install docker-compose -y
+fi
+
+echo " ** Building the docker image and starting the container..."
+docker-compose down && docker-compose up
+if [ $? -eq 0 ]; then
+    echo " ** Docker container started successfully"
+else
+    echo " ** Docker container failed to start"
 fi
