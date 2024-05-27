543861dc1a8b304306d2c8b09da365e706d9165d
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2024-05-27 12:04:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,13 +31,39 @@ echo " ** Building the docker image and starting the container..."
 docker-compose down && docker-compose up -d
 if [ $? -eq 0 ]; then
     echo " ** Docker container started successfully!"
-    echo ""
-    echo " Access web interface on port 5601"
-    echo " Go to Stack Management > Index Patterns"
-    echo " Create index pattern"
-    echo " Enter cowrie-* as the index pattern"
-    echo " Select @timestamp as the Time Filter"
-    echo ""
 else
     echo " ** Docker container failed to start"
 fi
+
+KIBANA_HOST="http://127.0.0.1:5601"
+INDEX_PATTERN_NAME="cowrie-*"
+TIME_FIELD="@timestamp"
+
+echo " ** Automatically adding index pattern"
+echo "Checking if Kibana is available at $KIBANA_HOST..."
+until curl --output /dev/null --silent --head --fail $KIBANA_HOST; do
+    echo "Waiting for Kibana to be available..."
+    sleep 5
+done
+echo "Kibana is available. Trying to create index pattern through the API via CURL..."
+
+# Create index pattern
+curl -X POST "$KIBANA_HOST/api/index_patterns/index_pattern" \
+     -H 'kbn-xsrf: true' \
+     -H 'Content-Type: application/json' \
+     -d '{
+           "index_pattern": {
+             "title": "'"$INDEX_PATTERN_NAME"'",
+             "timeFieldName": "'"$TIME_FIELD"'"
+           }
+         }'
+
+echo "Index pattern created: $INDEX_PATTERN_NAME"
+
+echo ""
+echo " Access web interface on port 5601"
+#    echo " Go to Stack Management > Index Patterns"
+#    echo " Create index pattern"
+#    echo " Enter cowrie-* as the index pattern"
+#    echo " Select @timestamp as the Time Filter"
+#    echo ""
