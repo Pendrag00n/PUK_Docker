57d2046f4600c877c57951112b3b3b44d0a50982
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2024-05-27 03:56:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,9 +16,16 @@ else
 fi
 
 echo " ** Building the docker image and starting the container..."
-docker-compose down && docker-compose up
+docker-compose down && docker-compose up -d
 if [ $? -eq 0 ]; then
     echo " ** Docker container started successfully"
+    echo ""
+    echo " Access web interface on port 5601"
+    echo " Go to Stack Management > Index Patterns"
+    echo " Create index pattern"
+    echo " Enter cowrie-* as the index pattern"
+    echo " Select @timestamp as the Time Filter"
+    echo ""
 else
     echo " ** Docker container failed to start"
 fi
